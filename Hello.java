6f4ea129f3d52def37448a42f5f3785b57f2035a
danielsahar15 <73643783+danielsahar15@users.noreply.github.com>
2023-12-06 17:46:57 +0200

    Delete Hello.java

--- Hello.java
+++
@@ -1,9 +0,0 @@
-@Path("/hello")
-public class GreetingResource {
-
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from RESTEasy Terasky";
-    }
-}

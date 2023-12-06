27bfc3a71e9b825b2c3ac6b031b8e674833bae95
danielsahar15 <73643783+danielsahar15@users.noreply.github.com>
2023-12-06 15:39:05 +0200

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
-public class HelloTeraSky {
-    public static void main(String[] args) {
-        System.out.println("Hello TeraSky!!"); 
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from RESTEasy Terasky";
     }
-} 
+}
